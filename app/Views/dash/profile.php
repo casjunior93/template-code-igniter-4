551590db1cd6bf0658c9aafb7016922bb2797895
Carlos Alberto Silva Júnior <56141054+casjunior93@users.noreply.github.com>
2022-09-01 21:03:58 -0300

    Add: formulário da página de perfil

--- app/Views/dash/profile.php
+++ app/Views/dash/profile.php
@@ -10,47 +10,28 @@
     <div class="col-8">
       <div class="profile-form">
         <form class="row g-3">
-          <div class="col-md-6">
-            <label for="inputEmail4" class="form-label">Email</label>
-            <input type="email" class="form-control" id="inputEmail4">
-          </div>
-          <div class="col-md-6">
-            <label for="inputPassword4" class="form-label">Password</label>
-            <input type="password" class="form-control" id="inputPassword4">
-          </div>
           <div class="col-12">
-            <label for="inputAddress" class="form-label">Address</label>
-            <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-          </div>
-          <div class="col-12">
-            <label for="inputAddress2" class="form-label">Address 2</label>
-            <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+            <label for="inputName" class="form-label">Nome</label>
+            <input type="text" class="form-control" id="inputName" placeholder="Nome" name="inputName">
           </div>
           <div class="col-md-6">
-            <label for="inputCity" class="form-label">City</label>
-            <input type="text" class="form-control" id="inputCity">
+            <label for="inputPhone" class="form-label">Telefone</label>
+            <input type="phone" name="phone" class="form-control" id="inputPhone" placeholder="Telefone">
           </div>
-          <div class="col-md-4">
-            <label for="inputState" class="form-label">State</label>
-            <select id="inputState" class="form-select">
-              <option selected>Choose...</option>
-              <option>...</option>
-            </select>
+          <div class="col-md-6">
+            <label for="inputEmail" class="form-label">Email</label>
+            <input type="email" name="email" class="form-control" id="inputEmail" placeholder="Email">
           </div>
-          <div class="col-md-2">
-            <label for="inputZip" class="form-label">Zip</label>
-            <input type="text" class="form-control" id="inputZip">
+          <div class="col-md-6">
+            <label for="inputPassword" class="form-label">Senha</label>
+            <input type="password" name="password" class="form-control" id="inputPassword">
           </div>
-          <div class="col-12">
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" id="gridCheck">
-              <label class="form-check-label" for="gridCheck">
-                Check me out
-              </label>
-            </div>
+          <div class="col-md-6">
+            <label for="inputPassword2" class="form-label">Repetir senha</label>
+            <input type="password" name="password2" class="form-control" id="inputPassword2">
           </div>
           <div class="col-12">
-            <button type="submit" class="btn btn-primary">Sign in</button>
+            <button type="submit" class="btn btn-primary">Salvar</button>
           </div>
         </form>
       </div>
