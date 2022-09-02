1071814e113f4fbb1a6b3f0391c737a739f897c6
Carlos Alberto Silva Júnior <56141054+casjunior93@users.noreply.github.com>
2022-09-02 08:38:29 -0300

    Add: Página de preços

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -38,6 +38,7 @@ $routes->set404Override();
 
 $routes->get('/', 'Home::index');
 $routes->get('/faqs', 'Home::faqs');
+$routes->get('/pricing', 'Home::pricing');
 
 //Authentication
 
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -34,6 +34,25 @@ class Home extends BaseController
 
         return view('templates/page/header', $data)
             . view('templates/external/header', $data)
+            . view('templates/external/secondary-header', $data)
+            . view($page)
+            . view('templates/page/footer');
+    }
+
+    public function pricing($page = 'pages/pricing')
+    {
+        if (!is_file(APPPATH . 'Views/' . $page . '.php')) {
+            // Whoops, we don't have a page for that!
+            throw new \CodeIgniter\Exceptions\PageNotFoundException($page);
+        }
+
+        $data = [
+            'title' => 'Preços'
+        ];
+
+        return view('templates/page/header', $data)
+            . view('templates/external/header', $data)
+            . view('templates/external/secondary-header', $data)
             . view($page)
             . view('templates/page/footer');
     }
